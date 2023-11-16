d2224829492bca2184bf4ed78b079e7a8caac276
Mohammad Biplob Molla <69385735+biplobmolla@users.noreply.github.com>
2023-11-16 21:29:58 +0600

    Basic Layout Done

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,18 @@
+import Navbar from "./_components/navbar";
+import Sidebar from "./_components/sidebar";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+      <div className="h-[80px] fixed w-full inset-y-0 md:pl-56 z-50">
+        <Navbar />
+      </div>
+      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+        <Sidebar />
+      </div>
+      <main className="md:pl-56 h-full">{children}</main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
